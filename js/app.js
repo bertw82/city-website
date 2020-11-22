747295af5bcb14ccdb99048955bef3b16739fcb4
Bert Witzel <bwitzel82@gmail.com>
2020-11-22 15:45:28 -0500

    create overlay navigation

--- js/app.js
+++ js/app.js
@@ -1,16 +1,7 @@
-const mediaQuery = window.matchMedia('(max-width: 425px)');
-const mainNav = document.getElementById('mainNav');
-const mainNavLi = mainNav.children;
-const mobileNav = document.getElementById('mobileNav');
-const mobileNavLi = mobileNav.children;
+function openNav() {
+  document.getElementById("mobileNav").style.width = "100%";
+}
 
-window.addEventListener('resize', () => {
-  if ( window.innerWidth < 425 ) {
-    for (let i = 0; i < mainNavLi.length; i++) {
-      mainNavLi[i].style.display = 'none';
-    }
-    for (let i = 0; i < mobileNavLi.length; i++) {
-      mobileNavLi[i].style.display = 'none';
-    }
-  };
-})
+function closeNav() {
+  document.getElementById("mobileNav").style.width = "0%";
+}
